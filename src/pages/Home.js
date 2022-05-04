355238e47d754e222d3eec8438f7c9f520b0d30a
Cyprien <roche.cyprien@gmail.com>
2022-05-04 14:15:18 +0200

    Design and add text to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,36 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemText from '@mui/material/ListItemText'
-import { menuItems } from '../components/MenuItems'
 
 export default function Home() {
   return (
     <Container>
-      <Typography
-        variant="h2" 
-        color="primary"
-        component="h1"
-        gutterBottom
-      >
-        Understanding the EN 301 549 standard
-      </Typography>
-      
-      <Typography praragraph>
-        This is the home page of a high-fidelity prototype tool designed to help people understand the EN 301 549 standard.
-      </Typography>
-      {/* <List>
-        {menuItems.map(item => (
-          <ListItem
-          key={item.text}
-          onClick={() => navigate(item.path)}
-          >
-          <ListItemText primary={item.text} />
-        </ListItem>
-        ))}
-      </List> */}
+        <Typography
+          variant="h2" 
+          color="primary"
+          component="h1"
+          gutterBottom
+        >
+          EN 301 549 Facilitator
+        </Typography>
 
+        <Typography paragraph>
+          The <b className='globalColor'>EN 301 549 Facilitator (ENF)</b> is a tool to help people <b>understand the EN 301 549 standard</b>.
+        </Typography>
+
+        <Typography paragraph>
+          Start using the ENF by clicking on the elements on the left hand side of the screen. 
+        </Typography>
+
+        <Typography paragraph>
+          This is a <i className='globalColor'>high-fidelity <b><u>prototype</u></b></i> built as part of a master thesis at the Universidad Politécnica de Madrid.
+          <br />
+          It is <b>not meant to be used in production</b> and is <b>not a real working product</b>.
+        </Typography>
+
+        <Typography paragraph>
+          If you have any questions, please email cyprienroche@duck.com.
+        </Typography>
     </Container>
   )
 }
