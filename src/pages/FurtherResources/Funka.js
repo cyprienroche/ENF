ed24d0ee481cc9cdf77e5a859b406f1b02612b72
Cyprien <roche.cyprien@gmail.com>
2022-05-04 18:57:54 +0200

    Remove redundant import of Link no longer used

--- src/pages/FurtherResources/Funka.js
+++ src/pages/FurtherResources/Funka.js
@@ -3,7 +3,6 @@ import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import { Link } from 'react-router-dom';
 
 export default function Funka() {
   return (
